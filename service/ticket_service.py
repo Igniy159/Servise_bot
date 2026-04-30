eebf175360f71036bd0edf28ad0e72163935cd86
Igniy <igniy159@gmail.com>
2026-05-01 05:47:27 +0700

    feat: The logic for checking the command and access to creating a request has been moved to the policy layer. deleted in core - validate event, full ticket validator, encode validator. refactor write ticket and escalation

--- service/ticket_service.py
+++ service/ticket_service.py
@@ -1,42 +1,65 @@
-from Service.Common import transactional,filters_validator,filters_key_validator,apply_scope
-from typing import Optional
-from Core.Ticket_core import User
-from Core.exceptions import PermissionDenied,ServiseValidationBreak, CoreValidationBreak,IncorrectWrite
-from Core.Ticket_core import validate_event, resolve_decision,Ticket,has_permission
-from Logger.logger import core_logger
-from Repository.write_model import ticket_assert,insert_history_record,ticket_update
-from Repository.read_model import get_users_with_data, get_tickets_with_data,fetch_history_ticket
+from service.common import transactional,filters_validator,filters_key_validator,apply_scope
+from core.ticket_core import User
+from core.exceptions import PermissionDenied,ServiseValidationBreak, CoreValidationBreak,IncorrectWrite
+from core.ticket_core import Ticket
+from policy.policy_ticket import PolicyObject, PolicyRequest, PolicyAlert
+from logger.logger import core_logger
+from repository.write_model import ticket_assert,insert_history_record,ticket_update
+from repository.read_model import get_users_with_data, get_tickets_with_data, fetch_history_ticket, get_branch_with_data
 from datetime import datetime
+from api.command import (CmdCreateTicket, CmdCloseTicket, CmdAssignTicket, CmdFinishTicket,
+CmdRejectTicket,CmdPriorityTicket,CmdConfirmTicket, CmdOnWaitingTicket,
+CmdOffWaitingTicket, QueryGetTicket, QueryGetHistoryTicket)
+
 
 @transactional
-def write_ticket(config:dict,
-                 user: Optional[User],
-                 event_data: dict,
-                 con=None)-> dict:
-
-    if not has_permission(user.role, "create_ticket", config):
-        raise PermissionDenied(f" User {user['user_name']} cannot create ticket")
-
-    if user.branch_id is None and 'branch_id' not in event_data:
-        raise ServiseValidationBreak("if changed not branch: in event need field branch_id")
-
+def write_ticket(user: User,
+                cmd: CmdCreateTicket,
+                config: dict,
+                con=None)-> tuple:
+#Определяем тип события
+    type_event = type_resolver(cmd,config['enum'])
+    if type_event == "OBJECT_PROBLEM":
+        event_control =  PolicyObject(user,cmd,config)
+    elif type_event == "REQUEST":
+        event_control = PolicyRequest(user,cmd,config)
+    else:
+        event_control = PolicyAlert(user,cmd,config)
+#Валидируем команду и принимаем решение (сценарий, отдел, приоритет)
     try:
-        validate_event(event_data, config)
+        resolve = event_control.policy_cmd()
     except CoreValidationBreak as e:
-        core_logger.error(f"Event {event_data['problem_name']} incorrect. {e}")
+        core_logger.error(f"Event {cmd['problem_name']} incorrect. {e}")
         raise
-
-
-    event_res = resolve_decision(event_data, config)
-
-    solution = escalation(config,event_data, user, con=con)
-
-    ticket = Ticket.from_created(event_res,config,user)
+    except PermissionDenied as e:
+        core_logger.error(f"Error in access user: {e}")
+        raise
+    if cmd.branch_id:
+        resolve['branch_name'] = get_branch_with_data(
+                                {'branch_id': cmd.branch_id},con=con
+                                )[0]['branch_name']
+#Собираем контекст из БД
+    manager = get_users_with_data({'user_activity': 1,
+                                   'role_id': 2,
+                                   'branch_id': user.branch_id},
+                                  con=con)
+    owner = get_users_with_data({'user_activity': 1,
+                                 'role_id': 4},
+                                con=con)
+    depart = get_users_with_data({'user_activity': 1,
+                                  'role_id': 3,
+                                  'depart_id': resolve['target_id']})
+    context = {'manager': manager,
+                'owner': owner,
+                'depart':depart}
+#Собираем из контекста правила эскалации(отдел, текущий статус, получателей)
+    solution, recipient = escalation(config['scenarios'],cmd,context)
+#Создаём тикет
+    ticket = Ticket.from_created(cmd,resolve,config,user)
     ticket.update_solution(solution)
     state_for_history = ticket.current_state
-
     ticket_for_db = Ticket.for_data_base(ticket,config)
-
+#Записываем тикет и историю в БД
     try:
         ticket_id = ticket_assert(ticket_for_db, con=con)
     except IncorrectWrite as e:
@@ -53,13 +76,10 @@ def write_ticket(config:dict,
     except IncorrectWrite as e:
         core_logger.error(f"History has not write. Reason: {e}")
         raise
-
-    api_recipient = solution['api_recipient']
+#Формируем событие и ответ для API
+    recipient['self'] = user.api_id
     event_alert = {'branch_name': ticket.branch_name,
                    'problem_name': ticket.problem_name,
-                   'self': user.api_id,
-                   'target':api_recipient.get('target'),
-                   'manager':api_recipient.get('manager'),
                    'problem_type': ticket.problem_type,
                    'ticket_id': ticket_id,
                    'branch_id': ticket.branch_id,
@@ -68,52 +88,86 @@ def write_ticket(config:dict,
                    'comment': ticket.comment,
                    'type': ticket.event_type,
                    'scenario': ticket.scenario}
-    return event_alert
+    return event_alert, recipient
 
-@transactional
-def escalation(config:dict,
-               event_data:dict,
-               user: Optional[User],
-               con=None):
-    manager = get_users_with_data(filter_value={'user_activity': 1, 'role_id': 2, 'branch_id': user.branch_id},con=con)
-    owner = get_users_with_data(filter_value={'user_activity': 1,'role_id': 4},con=con)
-    depart = get_users_with_data(filter_value={'user_activity': 1, 'role_id': 3, 'depart_id': event_data['target']})
-    need_confirm = config['scenarios']['SCENARIOS'][event_data['scenario']]["confirmation_required"]
+
+def type_resolver(cmd:CmdCreateTicket, enums:dict)->str:
+    """The function determines the event type through the config and returns it as a string"""
+    type_event = ""
+    if cmd.problem_category in enums['zones']:
+        type_event = ' OBJECT_PROBLEM'
+    elif cmd.problem_category in enums['REQUEST_CATEGORY']:
+        type_event = 'REQUEST'
+    elif cmd.problem_category in enums['CATEGORY_ALERTS']:
+        type_event = 'ALERT'
+    return type_event
+
+
+def escalation(scenarios: dict,
+               cmd: CmdCreateTicket,
+               context: dict)-> tuple:
+
+    need_confirm = scenarios['SCENARIOS'][cmd['scenario']]["confirmation_required"]
     solution ={}
+    recipient = {}
 
-    if not depart and not manager:
-        solution['assigned_to'] = owner[0]['user_id']
+    if not context['depart'] and not context['manager']:
+        solution['assigned_to'] = context['owner'][0]['user_id']
         solution['target'] = 'TOP_MANAGEMENT'
         solution['current_state'] = "IN_PROGRESS"
-        solution['api_recipient']['target'] = (owner[0]['api_user_id'])
+        recipient['target'] = context['owner'][0]['api_user_id']
 
-    elif not depart and manager and need_confirm:
+    elif not context['depart'] and context['manager'] and need_confirm:
         solution['target'] = 'TOP_MANAGEMENT'
         solution['current_state'] = "NEW"
-        solution['api_recipient']['manager'] = manager[0]['api_user_id']
+        recipient['manager'] = context['manager']['api_user_id']
 
-    elif not depart and manager and not need_confirm:
+    elif not context['depart'] and context['manager'] and not need_confirm:
         solution['target'] = 'TOP_MANAGEMENT'
         solution['current_state'] = "CONFIRMED"
-        solution['api_recipient']['manager'] = manager[0]['api_user_id']
-        solution['api_recipient']['target'] = [users['api_user_id'] for users in depart]
+        recipient['manager'] = context['manager'][0]['api_user_id']
+        recipient['target'] = [users['api_user_id'] for users in context['depart']]
 
-    elif not manager and depart:
+    elif not context['manager'] and context['depart']:
         solution['current_state'] = "CONFIRMED"
-        solution['api_recipient']['target'] = [users['api_user_id'] for users in depart]
+        recipient['target'] = [users['api_user_id'] for users in context['depart']]
 
-    elif manager and depart:
+    elif context['manager'] and context['depart']:
         solution['current_state'] = "NEW"
-        solution['api_recipient']['manager'] = manager[0]['api_user_id']
+        recipient['manager'] = context['manager'][0]['api_user_id']
 
-    return solution
+    return solution, recipient
 
 @transactional
-def apply_write_patch(config:dict, user:Optional[User], patch: dict, ticket_id: int, flag: str, con=None)-> dict:
+def apply_write_patch(user: User,
+                      cmd ,
+                      flag: str,
+                      con)-> dict:
     if user.role == 'OWNER':
         type_fsm = config['ticket_lifecycle']['ADMIN_LIFECYCLE']
     else:
         type_fsm = config['ticket_lifecycle']['NORMAL_LIFECYCLE']
+
+    # patch = {'current_state': 'CONFIRMED',
+    #          'comment': comment}
+    # patch = {'current_state': 'CANCELLED',
+    #          'reject_comment': reject_comment,
+    #          'date_close': datetime.now()}
+    # patch = {"priority": new_priority_id,
+    #          'comment': comment}
+    # patch = {'current_state': 'IN_PROGRESS',
+    #          'assigned_to': self.user.id,
+             # 'comment': comment}
+    # patch = {'current_state': 'WAITING_EXTERNAL',
+    #          'comment': comment}
+    # patch = {'current_state': 'IN_PROGRESS',
+    #          'comment': comment}
+    # patch = {'current_state': 'RESOLVED',
+    #          'comment': comment}
+    # patch = {'current_state': 'CLOSED',
+    #          'comment': comment,
+    #          'date_close': datetime.now()}
+
 
     if not has_permission(user.role, flag, config):
         raise PermissionDenied(f" User {user.name} cannot apply {flag}")
@@ -170,7 +224,9 @@ def apply_write_patch(config:dict, user:Optional[User], patch: dict, ticket_id: 
     return event_alert
 
 @transactional
-def receive_tickets(user:Optional[User], size=None, filters=None,con=None)-> list[dict]:
+def receive_tickets(user:User,
+                    cmd: QueryGetTicket,
+                    con)-> list[dict]:
     if filters:
         filters_key_validator(filters)
     filters_validator(user, filters)
@@ -186,7 +242,10 @@ def receive_tickets(user:Optional[User], size=None, filters=None,con=None)-> lis
     return res
 
 @transactional
-def receive_history(user:Optional[User], ticket_id, config, con=None):
+def receive_history(user:User,
+                    cmd: QueryGetHistoryTicket,
+                    config:dict,
+                    con):
     has_permission(user['role_name'],"history_vision",config)
     ticket = get_tickets_with_data({'ticket_id':ticket_id},con=con)[0]
     if user['branch_id'] is not None:
@@ -212,6 +271,7 @@ def full_view(ticket:dict, role: str):    #Построить модель дл�
         if key in need_field:
             ticket_view[key] = val
     return ticket_view
+
 def short_view(ticket: dict):
     need_field = {'ticket_id','date_create','branch_name','priority', "problem_name", "current_state"}
     ticket_view = {}
